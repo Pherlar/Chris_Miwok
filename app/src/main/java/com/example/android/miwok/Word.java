abbe041e1bb46f39cdc0a7ff9d40d83c65fae363
Chris <christopherlarmour@gmail.com>
2019-03-03 17:27:15 +1100

    Heaps of updates

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,22 +5,25 @@ public class Word {
     private String mDefaultTranslation;
     private String mMiwokTranslation;
     private int mImageResourceId = NO_IMAGE_PROVIDED;
+    private int mAudioResourceId;
     private static final int NO_IMAGE_PROVIDED = -1;
 
     //Create new word object that takes in a drawable resource
-    public Word (String defaultTranslation, String miwokTranslation,int imageResourceId)
+    public Word (String defaultTranslation, String miwokTranslation,int imageResourceId, int audioResourceId)
     {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
         mImageResourceId = imageResourceId;
+        mAudioResourceId = audioResourceId;
 
     }
 
-    //Create new word object
-    public Word (String defaultTranslation, String miwokTranslation)
+    //Version of Word class with no drawable resource i.e. Phrases activity
+    public Word(String defaultTranslation, String miwokTranslation, int audioResourceId)
     {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+        mAudioResourceId = audioResourceId;
     }
 
 
@@ -36,6 +39,8 @@ public class Word {
     public int getImageResourceId() {
         return mImageResourceId;
     }
+
+    public int getAudioResourceId(){return mAudioResourceId;}
 
 public boolean hasImage(){
         return mImageResourceId != NO_IMAGE_PROVIDED;
